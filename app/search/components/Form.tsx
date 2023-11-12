7dfd222c6d301aab8cd17ba4eac2ef5ee3643198
mnuralim <shadownur345.com>
2023-11-12 18:51:06 +0800

    update

--- app/search/components/Form.tsx
+++ app/search/components/Form.tsx
@@ -3,14 +3,12 @@ import React from "react";
 interface Props {
   name: string;
   setName: React.Dispatch<React.SetStateAction<string>>;
-  numberBarcode: string;
-  setNumberBarcode: React.Dispatch<React.SetStateAction<string>>;
   handleSearch: React.FormEventHandler<HTMLFormElement>;
 }
 
-const Form = ({ name, setName, numberBarcode, setNumberBarcode, handleSearch }: Props) => {
+const Form = ({ name, setName, handleSearch }: Props) => {
   return (
-    <form onSubmit={handleSearch} className="flex items-center justify-center gap-2 mt-16  px-2">
+    <form onSubmit={handleSearch} className="flex items-center justify-center gap-2 mt-16 mb-5 px-2">
       <div className=" relative w-full md:max-w-md">
         <input
           type="text"
@@ -18,19 +16,11 @@ const Form = ({ name, setName, numberBarcode, setNumberBarcode, handleSearch }: 
           className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
           placeholder="Nama produk"
           value={name}
+          autoFocus
           onChange={(e) => setName(e.target.value)}
         />
       </div>
-      <div className=" relative w-full md:max-w-md">
-        <input
-          type="text"
-          id="name"
-          className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-          placeholder="Nomor barcode"
-          value={numberBarcode}
-          onChange={(e) => setNumberBarcode(e.target.value)}
-        />
-      </div>
+
       <button
         className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
         type="submit"
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,7 +6,6 @@ import ProductCard from "../components/ProductCard";
 
 const Page = () => {
   const [name, setName] = useState<string>("");
-  const [numberBarcode, setNumberBarcode] = useState<string>("");
   const [allProducts, setAllProducts] = useState(products);
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
@@ -15,9 +14,8 @@ const Page = () => {
 
     const filterProduct = products.filter((el) => {
       const isNameMatch = name ? el.name.toLowerCase().includes(name.toLowerCase()) : true;
-      const isBarcodeMatch = numberBarcode ? el.qr.toLowerCase().includes(numberBarcode.toLowerCase()) : true;
 
-      return isNameMatch && isBarcodeMatch;
+      return isNameMatch;
     });
 
     setAllProducts(filterProduct);
@@ -27,10 +25,8 @@ const Page = () => {
 
   return (
     <div className="relative h-screen ">
-      <Form name={name} setName={setName} handleSearch={handleSearch} numberBarcode={numberBarcode} setNumberBarcode={setNumberBarcode} />
-      <p className="text-red-500 mb-5 px-2">
-        *<span className="text-white text-sm"> Filter dengan salah satu pilihan input diatas</span>
-      </p>
+      <Form name={name} setName={setName} handleSearch={handleSearch} />
+
       {allProducts.length === 0 && <p className="text-lg text-center text-white font-semibold mx-auto">Produk ini tidak masuk daftar boikot</p>}
       <div className="grid grid-cols-1 gap-2 md:grid-cols-4 mx-auto">
         {allProducts.map((data) => (
