66d93882527beb116bda10e3284eac425dc322a3
mnuralim <shadownur345.com>
2023-11-12 12:53:22 +0800

    fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ const Page = () => {
   const [scanResult, setScanResult] = useState(null);
 
   useEffect(() => {
-    const scanner = new Html5QrcodeScanner("reader", {
-      qrbox: {
-        width: 250,
-        height: 250,
+    const scanner = new Html5QrcodeScanner(
+      "reader",
+      {
+        qrbox: {
+          width: 250,
+          height: 250,
+        },
+        fps: 5,
       },
-      fps: 5,
-    });
+      undefined
+    );
 
     scanner.render(success, error);
 
