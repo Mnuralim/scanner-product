d09b1eaedfbde7d5906043479bfbaca8df5702f6
mnuralim <shadownur345.com>
2023-11-13 17:57:52 +0800

    fix layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,29 +2,24 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="  mx-2 mt-14 rounded-md shadow-md flex justify-center  w-full">
-      <div className="max-w-2xl bg-white bg-opacity-70 p-6 rounded">
+    <div className="mt-14 rounded-md shadow-md flex justify-center w-full px-2 overflow-x-hidden">
+      <div className="w-full md:max-w-2xl bg-white bg-opacity-70 p-6 rounded">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Tentang Kami</h1>
-
         <p className="text-gray-700 mb-4">Selamat datang di situs web kami! Kami adalah komunitas yang mendukung konsumenisme etis dan percaya pada membuat pilihan yang terinformasi ketika datang ke dukungan terhadap bisnis dan produk.</p>
-
         <p className="text-gray-700 mb-4">
           Misi kami adalah untuk meningkatkan kesadaran tentang produk atau perusahaan yang mungkin terlibat dalam praktik yang merugikan bagi masyarakat, lingkungan, atau melanggar standar etika. Dengan memboikot produk tersebut, kami
           bertujuan untuk mendorong praktik bisnis yang bertanggung jawab dan berkelanjutan.
         </p>
-
         <h2 className="text-2xl font-bold text-gray-800 mb-2">Mengapa Boikot?</h2>
         <p className="text-gray-700 mb-4">
           Boikot adalah alat yang kuat untuk menyatakan ketidakpuasan terhadap tindakan atau kebijakan suatu perusahaan. Ini mengirim pesan jelas bahwa konsumen peduli tentang pertimbangan etis dan bersedia membuat pilihan sesuai dengan
           nilai-nilai mereka.
         </p>
-
         <h2 className="text-2xl font-bold text-gray-800 mb-2">Terlibatlah</h2>
         <p className="text-gray-700 mb-4">
           Kami mengajak Anda bergabung dengan komunitas kami dan tetap terinformasi tentang konsumenisme yang bertanggung jawab. Dengan berbagi informasi, meningkatkan kesadaran, dan membuat pilihan etis bersama-sama, kita dapat
           berkontribusi pada dunia yang lebih berkelanjutan dan adil.
         </p>
-
         <p className="text-gray-700 mb-4">Bersama-sama, kita dapat membuat perubahan. Terima kasih telah menjadi bagian dari gerakan kami!</p>
       </div>
     </div>
