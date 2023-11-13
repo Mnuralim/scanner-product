859124b631aa8ed17849fe24c8b6d34843ce3205
mnuralim <shadownur345.com>
2023-11-13 17:53:42 +0800

    change font size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,16 +8,16 @@ const Navbar = () => {
   return (
     <header className="absolute top-0 left-0 w-full z-20">
       <nav className="flex flex-wrap justify-center items-center px-3 gap-3 mt-5 md:justify-center md:gap-14">
-        <Link href={"/"} className="text-sm md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
+        <Link href={"/"} className="text-base md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
           Beranda
         </Link>
-        <Link href={"/search"} className="text-sm md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
+        <Link href={"/search"} className="text-base md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
           Cari
         </Link>
-        <Link href={"/scan"} className="text-sm md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
+        <Link href={"/scan"} className="text-base md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
           Scan
         </Link>
-        <Link href={"/about"} className="text-sm md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
+        <Link href={"/about"} className="text-base md:text-lg text-white uppercase cursor-pointer hover:text-gray-300">
           Tentang
         </Link>
       </nav>
