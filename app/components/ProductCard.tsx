e1ef4b79561e09fc5590d70b059a9d48718e674f
mnuralim <shadownur345.com>
2023-11-14 17:40:00 +0800

    update

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,10 +2,21 @@ import React from "react";
 
 interface Props {
   product: IProduct;
+  isLoading: boolean;
 }
 
-const ProductCard = ({ product }: Props) => {
-  return (
+const ProductCard = ({ product, isLoading }: Props) => {
+  return isLoading || !product ? (
+    <div className="max-w-md bg-white bg-opacity-75 rounded-md overflow-hidden shadow-md mx-2">
+      <div className="w-full h-48 object-cover bg-gray-200 rounded-md"></div>
+      <div className="p-4">
+        <h2 className="text-xl font-semibold h-2 w-full bg-gray-200 rounded-md"></h2>
+        <p className="text-sm h-2 w-full bg-gray-200 rounded-md">Brand: XYZ</p>
+        <p className="text-sm h-2 w-full bg-gray-200 rounded-md">Asal: </p>
+        <p className="text-gray-700 mt-2 h-2 w-full bg-gray-200 rounded-md"></p>
+      </div>
+    </div>
+  ) : (
     <div className="max-w-md bg-white bg-opacity-75 rounded-md overflow-hidden shadow-md mx-2">
       <img className="w-full h-48 object-cover" src={product.imageUrl} alt="Product Image" />
       <div className="p-4">
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,13 +4,15 @@ import Form from "./components/Form";
 import ProductCard from "../components/ProductCard";
 import { useProduct } from "@/utils/swr";
 import { KeyedMutator } from "swr";
+import { infinity } from "ldrs";
+
+infinity.register();
 
 const Page = () => {
   const [name, setName] = useState<string>("");
   const [query, setQuery] = useState<string>("");
 
   const { product, isLoading, mutate }: { product: IProduct[]; isLoading: boolean; mutate: KeyedMutator<any> } = useProduct(undefined, { name: query });
-  console.log(product);
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,19 +21,22 @@ const Page = () => {
     } else {
       setQuery("");
     }
-    mutate();
   };
 
-  if (isLoading) return <p>loading...</p>;
-
+  if (isLoading || !product)
+    return (
+      <div className="h-screen inset-0 bg-black bg-opacity-40 w-full flex items-center justify-center">
+        <l-infinity size="100" stroke="4" stroke-length="0.15" bg-opacity="0.1" speed="1.3" color="white"></l-infinity>
+      </div>
+    );
   return (
     <div className="relative flex justify-center w-full">
       <div>
         <Form name={name} setName={setName} handleSearch={handleSearch} />
-        {product.length === 0 && <p className="text-lg text-center text-white font-semibold mx-auto">Produk ini tidak masuk daftar boikot</p>}
+        {product?.length === 0 && <p className="text-lg text-center text-white font-semibold mx-auto">Produk ini tidak masuk daftar boikot</p>}
         <div className="grid grid-cols-1 gap-3 md:grid-cols-4 ">
           {product?.map((data) => (
-            <ProductCard key={data._id} product={data} />
+            <ProductCard key={data._id} product={data} isLoading={isLoading} />
           ))}
         </div>
       </div>
